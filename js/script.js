d5fc9fc95a5dbd88b89d88679f4a1ee131ca71c3
edinsonojeda <ojedamorenoedinson@gmail.com>
2024-08-30 14:41:35 -0500

    adivina el numero

---
+++ js/script.js
@@ -0,0 +1,69 @@
+let numrandom = Math.floor(Math.random() * 100) + 1;
+
+const resultado = document.querySelector(".resultado");
+const resAnterior = document.querySelector(".previa");
+const ayuda = document.querySelector(".pista");
+const btn = document.querySelector(".btnEnviar");
+const num = document.querySelector(".numero");
+
+let cont = 1;
+let resetButton;
+
+function juega() {
+    let numUsuario = Number(num.value);
+
+    if (cont === 1) {
+        resAnterior.textContent = "Números anteriores: ";
+    }
+    resAnterior.textContent += numUsuario + " _ ";
+
+    if (numUsuario === numrandom) {
+        resultado.textContent = "¡Felicidades! ¡Lo adivinaste!";
+        resultado.style.backgroundColor = "green";
+        ayuda.textContent = "";
+        finJuego();
+    } else if (cont === 10) {
+        resultado.textContent = "¡¡¡Fin del juego!!!";
+        finJuego();
+    } else {
+        resultado.textContent = "¡Incorrecto!";
+        resultado.style.backgroundColor = "red";
+        if (numUsuario < numrandom) {
+            ayuda.textContent = "¡El número es más grande!";
+        } else if (numUsuario > numrandom) {
+            ayuda.textContent = "¡El número es más pequeño!";
+        }
+    }
+    cont++;
+    num.value = "";
+    num.focus();
+}
+
+btn.addEventListener("click", juega);
+
+function finJuego() {
+    num.disabled = true;
+    btn.disabled = true;
+    resetButton = document.createElement("button");
+    resetButton.textContent = "Volver a Jugar";
+    const formulario = document.querySelector(".fom");
+    formulario.appendChild(resetButton);
+    resetButton.addEventListener("click", resetJuego);
+}
+
+function resetJuego() {
+    cont = 1;
+    const mensajes = document.querySelectorAll(".mensajes p");
+    for (let i = 0; i < mensajes.length; i++) {
+        mensajes[i].textContent = "";
+    }
+    resetButton.parentNode.removeChild(resetButton);
+
+    num.disabled = false;
+    btnEnviar.disabled = false;//error
+    num.value = "";
+    num.focus();
+
+    resultado.style.backgroundColor = "white";
+    numrandom = Math.floor(Math.random() * 100) + 1;
+}
